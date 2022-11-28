e25f680083468627fa1e291fe395185980f0d462
ahmed-elsowy <elsowy2017@gmail.com>
2022-11-28 14:34:51 +0200

    28-11-2022

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+//check idf there is color in local storage 
+if(localStorage.getItem("color-option")   !== null){
+    //set selected color to root 
+    document.documentElement.style.setProperty('--main-color' , localStorage.getItem("color-option"))
+
+    //remove active class from all li
+    document.querySelectorAll(".colors li").forEach(e =>{
+        e.classList.remove("active")
+
+        //add sctive class to element with data-color === selected color in local storage
+        if(e.dataset.color === localStorage.getItem("color-option")){
+            e.classList.add("active")
+        }
+    })
+
+
+}
+
 let toggleMenue = document.querySelector(".toggle-menue")
 let lists =  document.querySelector(".lists")
 
@@ -37,25 +55,73 @@ if we neglect this function => when we click on li in lists the menue disappear
 lists.onclick = function(e){
     e.stopPropagation()
 }
-
+//////////////////////////////Background ///////////////////////
 
 //change Background
 
-// let landingPage = document.querySelector(".landingPage")
-// let arrayImg = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"]
+let landingPage = document.querySelector(".landingPage")
+let arrayImg = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"]
 
-// function randomizeImages(){
+function randomizeImages(){
     
-//      setInterval( ()=>{
-//         let randomValue = Math.floor( Math.random() * arrayImg.length )
-//         landingPage.style.backgroundImage = `url("images/${arrayImg[randomValue]}")`;
-//     },1000 )
-        
-// }
-// randomizeImages()
+    backgroundInterval = setInterval( ()=>{
+    let randomValue = Math.floor( Math.random() * arrayImg.length )
+    landingPage.style.backgroundImage = `url("images/${arrayImg[randomValue]}")`;
+},1000 )
+    
+}
+randomizeImages()
+
+let backgroundOption = true;
+
+
+let backgroundLocalItem = localStorage.getItem("backgroundOption")
+
+
+//check if background is not empty
+if(backgroundLocalItem !== null){
+
+    document.querySelectorAll(".random-backgrounds span").forEach((e)=>{
+        e.classList.remove("active")
+    })
+    if(backgroundLocalItem === "true"){
+        // backgroundOption = true
+        randomizeImages()
+        document.querySelector(".random-backgrounds .yes").classList.add("active")
+    }else{
+        // backgroundOption = false
+        clearInterval(backgroundInterval)
+        document.querySelector(".random-backgrounds .no").classList.add("active")
+    }
+
+}
 
 
 
+//switch random background using option box
+
+let randomBackgrounds = document.querySelectorAll(".random-backgrounds span")
+//loop on all span
+randomBackgrounds.forEach(span => {
+    //click on every span
+    span.addEventListener("click",(e)=>{
+
+        //control active
+       handleActive(span)
+       if(e.target.dataset.background === "yes"){
+            backgroundOption = true;
+            randomizeImages()
+            localStorage.setItem("backgroundOption",true)
+       }else{
+        backgroundOption = false;
+        clearInterval(backgroundInterval)
+        localStorage.setItem("backgroundOption",false)
+       }
+    })
+    
+})
+
+////////////////////////End Background///////////////////////////
 /////////// click on setting gear//////////
 
 let gear = document.querySelector(".gear") //icon
@@ -67,13 +133,42 @@ gear.onclick=function(e){
    
     toggleSetting.parentElement.classList.toggle("openSettings")
     // toggleSetting.parentElement.style.left = "-200px";
-    
+
     // e.stopPropagation()
 }
 
+//change color
+let colorsLi = document.querySelectorAll(".colors li")
+
+colorsLi.forEach( li => {
+    
+    li.addEventListener("click" , (e)=>{
+        document.documentElement.style.setProperty('--main-color' , e.target.dataset.color)
+        
+
+        //control active class//////////      
+        colorsLi.forEach(e =>{
+            e.classList.remove("active")
+        })
+        //add to the selected li element
+        e.target.classList.add("active")
+
+        //set color on local storage
+        localStorage.setItem("color-option" , e.target.dataset.color)
+
+    })
+})
 
 
+///function to control active
 
+function handleActive(e){
+    //remove clas active from all span
+    e.parentElement.querySelectorAll(".active").forEach(e=>{
+       e.classList.remove("active")
+      })
+      e.classList.add("active")
+} 
 
 
 
